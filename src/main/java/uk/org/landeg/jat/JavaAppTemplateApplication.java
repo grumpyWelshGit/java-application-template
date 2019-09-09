b123655bc14967b3e44db8ac4990c04b491dab68
Andrew Landeg <andys.junk.76@gmail.com>
2019-09-09 23:04:22 +0100

    war config added

--- src/main/java/uk/org/landeg/jat/JavaAppTemplateApplication.java
+++ src/main/java/uk/org/landeg/jat/JavaAppTemplateApplication.java
@@ -2,9 +2,10 @@ package uk.org.landeg.jat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JavaAppTemplateApplication {
+public class JavaAppTemplateApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaAppTemplateApplication.class, args);
